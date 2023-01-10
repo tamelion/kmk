687d6450492206103a2c7be2f29b51724cb4fb4c
Jim Perry <mail@jim.nz>
2023-01-10 21:53:32 +1300

    Combos FTW!

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from kb import KMKKeyboard
 
 from kmk.keys import KC
-from kmk.modules.combos import Chord, Combos
-from kmk.modules.layers import Layers
 from kmk.extensions.media_keys import MediaKeys
+
+from kmk.modules.combos import Chord, Combos, Sequence
+from kmk.modules.holdtap import HoldTapRepeat
+from kmk.modules.layers import Layers
 from kmk.modules.modtap import ModTap
 from kmk.modules.mouse_keys import MouseKeys
-from kmk.modules.oneshot import OneShot
-
 
 keyboard = KMKKeyboard()
 
@@ -16,22 +16,13 @@ layers = Layers()
 mediakeys = MediaKeys()
 mousekeys = MouseKeys()
 modtap = ModTap()
-oneshot = OneShot()
 
-keyboard.modules = [combos, layers, mediakeys, modtap, mousekeys, oneshot]
+# Config for modtap and layertap
+modtap.tap_time = 300
+modtap.tap_interrupted = True
+modtap.repeat = HoldTapRepeat.ALL
 
-# OneShot mods
-OS_LCTL = KC.OS(KC.LCTL, tap_time=None)
-OS_LSFT = KC.OS(KC.LSFT, tap_time=None)
-OS_LGUI = KC.OS(KC.LGUI, tap_time=None)
-OS_LALT = KC.OS(KC.LALT, tap_time=None)
-
-OS_LCTL_LSFT = KC.OS(KC.LCTL(OS_LSFT), tap_time=None)
-OS_LCTL_LALT = KC.OS(KC.LCTL(OS_LALT), tap_time=None)
-OS_LCTL_LGUI = KC.OS(KC.LCTL(OS_LGUI), tap_time=None)
-OS_LSFT_LALT = KC.OS(KC.LSFT(OS_LALT), tap_time=None)
-OS_LSFT_LGUI = KC.OS(KC.LSFT(OS_LGUI), tap_time=None)
-OS_LALT_LGUI = KC.OS(KC.LALT(OS_LGUI), tap_time=None)
+keyboard.modules = [combos, layers, mediakeys, modtap, mousekeys]
 
 # Shortcuts
 UNDO = KC.LCTL(KC.Z)
@@ -44,28 +35,27 @@ SAVE = KC.LCTRL(KC.S)
 LOCK = KC.LWIN(KC.L)
 
 # Layers
-SPC_NAV = KC.LT(2, KC.SPACE, prefer_hold = True)
-TAB_SYM = KC.LT(3, KC.TAB, prefer_hold = True)
-SWB_NUM = KC.LT(4, KC.NO, prefer_hold = True)
-SWF_EXT = KC.LT(5, KC.NO, prefer_hold = True)
+SPC_NAV = KC.LT(1, KC.SPACE)
+TAB_SYM = KC.LT(3, KC.TAB)
+SWB_NUM = KC.LT(4, KC.NO)
+SWF_EXT = KC.LT(5, KC.NO)
 
 # Mod taps
-BSPC_SH = KC.MT(KC.BSPACE, KC.RSHIFT, tap_interrupted=True)
-CMD_SH = KC.MT(KC.LCMD, KC.RSHIFT, tap_interrupted=True)
-Z_SH = KC.MT(KC.Z, KC.LSHIFT, tap_interrupted=True)
-SL_SH = KC.MT(KC.SLSH, KC.RSHIFT, tap_interrupted=True)
-#US_SH = KC.MT(KC.UNDS, KC.RSHIFT, tap_interrupted=True)
-ZERO_SH = KC.MT(KC.N0, KC.RSHIFT, tap_interrupted=True)
-ENT_CTL = KC.MT(KC.ENT, KC.RCTL, tap_interrupted=True)
-DEL_ALT = KC.MT(KC.DEL, KC.RALT, tap_interrupted=True)
+BSPC_SH = KC.MT(KC.BSPACE, KC.RSHIFT)
+CMD_SH = KC.MT(KC.LCMD, KC.RSHIFT)
+Z_SH = KC.MT(KC.Z, KC.LSHIFT)
+SL_SH = KC.MT(KC.SLSH, KC.RSHIFT)
+#US_SH = KC.MT(KC.UNDS, KC.RSHIFT)
+ZERO_SH = KC.MT(KC.N0, KC.RSHIFT)
 
-TRACK = KC.MO(6) # For trackball, later
+TRACK = KC.MO(2) # For trackball, later
 CANT = KC.NO # Impossible (do not map this)
 
 combos.combos = [
-    Chord((OS_LCTL, OS_LALT), OS_LCTL_LALT, timeout=1000),
-    Chord((OS_LCTL, OS_LSFT), OS_LCTL_LSFT, timeout=1000),
-
+    Sequence((KC.COMM, KC.J), KC.ESC, timeout=200),
+    #Chord((KC.L, KC.QUOT), KC.ENT),
+    Chord((KC.A, KC.S), KC.LALT),
+    Chord((KC.D, KC.F), KC.LCTL),
 ]
 
 keyboard.keymap = [
@@ -73,19 +63,19 @@ keyboard.keymap = [
         KC.Q,       KC.W,       KC.E,       KC.R,       KC.T,               KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,
         KC.A,       KC.S,       KC.D,       KC.F,       KC.G,               KC.H,       KC.J,       KC.K,       KC.L,       KC.QUOT,
         Z_SH,       KC.X,       KC.C,       KC.V,       KC.B,               KC.N,       KC.M,       KC.COMM,    KC.DOT,     SL_SH,
-                                KC.ESC,     SPC_NAV,    KC.TAB,             OS_LALT,    OS_LSFT,    OS_LCTL,             
+                                KC.TAB,     SPC_NAV,    TRACK,              TRACK,      BSPC_SH,    KC.ENT,
     ],
-    [   # 1 - COLEMAK-DH
-        KC.Q,       KC.W,       KC.F,       KC.P,       KC.B,               KC.J,       KC.L,       KC.U,       KC.Y,       KC.QUOT,
-        KC.A,       KC.R,       KC.S,       KC.T,       KC.G,               KC.M,       KC.N,       KC.E,       KC.I,       KC.O,
-        KC.Z,       KC.X,       KC.D,       KC.V,       KC.Z,               KC.K,       KC.H,       KC.COMM,    KC.DOT,     KC.SLSH,
-                                KC.NO,      KC.NO,      KC.NO,              KC.NO,      KC.NO,      TRACK,
-    ],
+#    [   # 1 - COLEMAK-DH
+#        KC.Q,       KC.W,       KC.F,       KC.P,       KC.B,               KC.J,       KC.L,       KC.U,       KC.Y,       KC.QUOT,
+#        KC.A,       KC.R,       KC.S,       KC.T,       KC.G,               KC.M,       KC.N,       KC.E,       KC.I,       KC.O,
+#        KC.Z,       KC.X,       KC.D,       KC.V,       KC.Z,               KC.K,       KC.H,       KC.COMM,    KC.DOT,     KC.SLSH,
+#                                KC.TAB,     SPC_NAV,    TRACK,              TRACK,      BSPC_SH,    TRACK,
+#    ],
     [   # 2 - Nav
         KC.N1,      KC.N2,      KC.N3,      KC.N4,      KC.N5,              KC.N6,      KC.N7,      KC.N8,      KC.N9,      KC.N0,  
         KC.TILD,    KC.HOME,    KC.PGDN,    KC.PGUP,    KC.END,             KC.LEFT,    KC.DOWN,    KC.UP,      KC.RGHT,    KC.SCLN,
         UNDO,       CUT,        COPY,       PASTE,      REDO,               KC.MINS,    KC.EQL,     KC.LBRC,    KC.RBRC,    KC.BSLS,  
-                                CANT,       CANT,       CANT,               OS_LALT,    OS_LSFT,    OS_LCTL,              
+                                CANT,       CANT,       CANT,               TRACK,      BSPC_SH,    TRACK,
     ],
 #    [   # 3 - Sym 
 #        KC.TILD,    KC.NO,      KC.NO,      KC.NO,      KC.NO,              KC.CIRC,    KC.PIPE,    KC.EXLM,    KC.AMPR,    KC.COLN, 
@@ -105,12 +95,12 @@ keyboard.keymap = [
 #        KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,              KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,  
 #                                CANT,       CANT,       CANT,               CANT,       CANT,       TRACK,
 #    ],
-#    [   # 6 - Mouse
-#        KC.NO,      KC.NO,      KC.MS_UP,   KC.NO,      KC.NO,              KC.NO,      KC.NO,      KC.MB_MMB,  KC.NO,      KC.NO,  
-#        KC.NO,      KC.MS_LT,   KC.MS_DN,   KC.MS_RT,   KC.NO,              KC.NO,      KC.MB_LMB,  KC.MB_RMB,  KC.NO,      KC.NO,  
-#        KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,              KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,  
-#                                KC.NO,      KC.NO,      KC.NO,              CANT,       CANT,       CANT,
-#    ],
+    [   # 6 - Mouse
+        KC.NO,      KC.NO,      KC.MS_UP,   KC.NO,      KC.NO,              KC.NO,      KC.NO,      KC.MB_MMB,  KC.NO,      KC.NO,  
+        KC.NO,      KC.MS_LT,   KC.MS_DN,   KC.MS_RT,   KC.NO,              KC.NO,      KC.MB_LMB,  KC.MB_RMB,  KC.NO,      KC.NO,  
+        KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,              KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,  
+                                KC.NO,      KC.NO,      KC.NO,              CANT,       CANT,       CANT,
+    ],
 ]
 
 if __name__ == '__main__':
