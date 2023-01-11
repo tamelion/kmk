bdff9d6574085b1282fbd3f15150cfbeb28bada7
Jim Perry <mail@jim.nz>
2023-01-11 22:09:32 +1300

    git: remaps and chords

--- main.py
+++ main.py
@@ -34,16 +34,14 @@ STAB = KC.LSFT(KC.TAB)
 SAVE = KC.LCTRL(KC.S)
 LOCK = KC.LWIN(KC.L)
 
-
+# Navigation
 TABP = KC.LCTL(KC.PGUP)
 TABN = KC.LCTL(KC.PGDN)
-WINP = KC.LALT(KC.TAB)
-WINN = KC.LALT(KC.TAB)
 
 # Layers
 SPC_NAV = KC.LT(2, KC.SPACE, prefer_hold=True)
-TAB_SYM = KC.LT(3, KC.NO, prefer_hold=True)
-ENT_NUM = KC.LT(4, KC.ENT, prefer_hold=True)
+ENT_SYM= KC.LT(3, KC.ENT, prefer_hold=True)
+TAB_NUM= KC.LT(4, KC.TAB, prefer_hold=True)
 SWF_EXT = KC.LT(5, KC.NO, prefer_hold=True)
 
 # Mod taps
@@ -54,7 +52,7 @@ TRACK = KC.MO(6) # For trackball, later
 CANT = KC.NO # Impossible (do not map this)
 
 combos.combos = [
-    Chord((KC.COMM, KC.J), KC.ESC)
+    Sequence((KC.COMM, KC.J), KC.ESC, timeout=200),
     #Chord((KC.L, KC.QUOT), KC.ENT),
 
     # LHS
@@ -69,38 +67,42 @@ combos.combos = [
     Chord((20,21), KC.LGUI, match_coord=True),
     Chord((18,19,20), KC.RCTL(KC.RALT), match_coord=True),
     Chord((19,20,21), KC.RALT(KC.LGUI), match_coord=True),
+
+    Chord((KC.COMMA, KC.DOT), KC.EXLM),
+    Chord((KC.DOT, KC.SLSH), KC.COLN),
 ]
+
 
 keyboard.keymap = [
     [   # 0 - QWERTY
         KC.Q,       KC.W,       KC.E,       KC.R,       KC.T,               KC.Y,       KC.U,       KC.I,       KC.O,       KC.P,
         KC.A,       KC.S,       KC.D,       KC.F,       KC.G,               KC.H,       KC.J,       KC.K,       KC.L,       KC.QUOT,
         KC.Z,       KC.X,       KC.C,       KC.V,       KC.B,               KC.N,       KC.M,       KC.COMM,    KC.DOT,     KC.SLSH,
-                                TAB_SYM,    SPC_NAV,    TRACK,              TRACK,      BSPC_SH,    ENT_NUM,
+                                TAB_NUM,    SPC_NAV,    TRACK,              KC.NO,      BSPC_SH,    ENT_SYM,
     ],
     [   # 1 - COLEMAK-DH
         KC.Q,       KC.W,       KC.F,       KC.P,       KC.B,               KC.J,       KC.L,       KC.U,       KC.Y,       KC.QUOT,
         KC.A,       KC.R,       KC.S,       KC.T,       KC.G,               KC.M,       KC.N,       KC.E,       KC.I,       KC.O,
         KC.Z,       KC.X,       KC.D,       KC.V,       KC.Z,               KC.K,       KC.H,       KC.COMM,    KC.DOT,     KC.SLSH,
-                                KC.TAB,     SPC_NAV,    TRACK,              TRACK,      BSPC_SH,    TRACK,
+                                TAB_NUM,    SPC_NAV,    TRACK,              KC.NO,      BSPC_SH,    ENT_SYM,
     ],
     [   # 2 - Nav
-        KC.N1,      KC.N2,      WINP,       WINN,       KC.N5,              KC.N6,      KC.N7,      KC.N8,      KC.N9,      KC.N0,  
+        KC.N1,      KC.N2,      KC.N3,      KC.N4,      KC.N5,              KC.N6,      KC.N7,      KC.N8,      KC.N9,      KC.N0,  
         KC.TILD,    KC.HOME,    TABP,       TABN,       KC.END,             KC.LEFT,    KC.DOWN,    KC.UP,      KC.RGHT,    KC.SCLN,
         UNDO,       CUT,        COPY,       PASTE,      REDO,               KC.MINS,    KC.EQL,     KC.LBRC,    KC.RBRC,    KC.BSLS,  
-                                CANT,       CANT,       CANT,               TRACK,      BSPC_SH,    TRACK,
+                                CANT,       CANT,       CANT,               KC.NO,      KC.NO,      KC.NO,
     ],
     [   # 3 - Sym 
         KC.EXLM,    KC.AT,      KC.HASH,    KC.DLR,     KC.PERC,            KC.CIRC,    KC.AMPR,    KC.ASTR,    KC.MINS,    KC.PLUS, 
-        KC.TILD,    KC.HASH,    KC.LCBR,    KC.LPRN,    KC.LBRC,            KC.GRV,     KC.LABK,    KC.EQL,     KC.RABK,    KC.COLN,
-        KC.NO,      KC.NO,      KC.RCBR,    KC.RPRN,    KC.RBRC,            KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.QUES,  
-                                CANT,       CANT,       CANT,               STAB,       KC.EXLM,    TRACK,
+        KC.TILD,    KC.LBRC,    KC.LCBR,    KC.LPRN,    KC.NO,              KC.GRV,     KC.RPRN,    KC.RCBR,    KC.RBRC,    KC.COLN,
+        KC.NO,      KC.LABK,    KC.EQL,     KC.RABK,    KC.NO,              KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.QUES,  
+                                SWF_EXT,    KC.UNDS,    TRACK,              CANT,       CANT,       CANT,
     ],
     [   # 4 - Num
         KC.N1,      KC.N2,      KC.N3,      KC.N4,      KC.N5,              KC.N6,      KC.N7,      KC.N8,      KC.N9,      KC.N0,  
-        KC.PSLS,    KC.PMNS,    KC.EQL,     KC.PPLS,    KC.PAST,            KC.DLR,     KC.N4,      KC.N5,      KC.N6,      KC.DOT,
-        KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,              KC.NO,      KC.N1,      KC.N2,      KC.N3,      KC.NO,  
-                                CANT,       CANT,       CANT,               KC.ESC,     ZERO_SH,    TRACK,
+        KC.PSLS,    KC.PMNS,    KC.PPLS,    KC.PAST,    KC.EQL,             KC.DLR,     KC.N4,      KC.N5,      KC.N6,      KC.EQL,
+        KC.NO,      KC.NO,      KC.NO,      KC.NO,      KC.NO,              KC.NO,      KC.N1,      KC.N2,      KC.N3,      KC.DOT,  
+                                CANT,       CANT,       CANT,               TRACK,      KC.N0,      SWF_EXT,
     ],
     [   # 5 - Ext
         KC.DF(0),   KC.NO,      KC.NO,      KC.NO,      KC.NO,              KC.NO,      KC.BRID,    KC.BRIU,    KC.NO,      KC.DF(1),  
